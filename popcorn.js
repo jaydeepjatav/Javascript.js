a5c41548bd8e476a7970dd2545620df612292e0d
JAYDEEP KUMAR <157721833+jaydeepjatav@users.noreply.github.com>
2025-02-06 17:05:01 +0530

    popcorn.js

---
+++ popcorn.js
@@ -0,0 +1,13 @@
+let size='XL';
+if(size==='Extra large'){
+    console.log('Size is Extra large');
+}
+else if(size==='large'){
+    console.log('Size is large');
+}
+if(size==='medium'){
+    console.log('Size is medium');
+}
+if(size==='small'){
+    console.log('Size is small');
+}
